cd593218e278e50e722aab1fabd398c003896e24
Gear <84141000+macgeargear@users.noreply.github.com>
2024-05-13 14:26:28 +0700

    feat: dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
+import Dashboard from "@/components/Dashboard";
 import LineChart from "@/components/LineChart";
 import MaxWidthWrapper from "@/components/MaxwidthWrapper";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-5xl">Embeded Final lnwza</h1>
-      <MaxWidthWrapper className="pb-24 pt-10 lg:grid lg:grid-cols-3 sm:pb-32 lg:gap-x-0 xl:gap-x-8 lg:pt-24 xl:pt-32 lg:pb-52">
-        <LineChart />
+      <MaxWidthWrapper className="">
+        <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
+          <Dashboard />
+          <LineChart />
+        </div>
       </MaxWidthWrapper>
     </main>
   );
