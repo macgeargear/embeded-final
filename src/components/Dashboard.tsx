db1bbbff8d0f1099165026a4b6814e93c40b343e
Gear <84141000+macgeargear@users.noreply.github.com>
2024-05-13 14:32:21 +0700

    chore

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -5,39 +5,13 @@ import { Icons } from "./Icons";
 
 type Props = {};
 
-const DASHBOARD_INFO: {
-  title: string;
-  value: number;
-  icon: React.JSX.Element;
-}[] = [
-  {
-    title: "PM 2.5 level",
-    value: 189,
-    icon: Icons.wind(),
-  },
-  {
-    title: "PM 2.5 level",
-    value: 189,
-    icon: Icons.wind(),
-  },
-  {
-    title: "PM 2.5 level",
-    value: 189,
-    icon: Icons.wind(),
-  },
-  {
-    title: "PM 2.5 level",
-    value: 189,
-    icon: Icons.wind(),
-  },
-];
-
 export default function Dashboard({}: Props) {
   return (
     <MaxWidthWrapper className="grid grid-cols-2 gap-4">
-      {DASHBOARD_INFO.map((info, index) => (
-        <DashboardCard key={index} {...info} />
-      ))}
+      <DashboardCard title="PM 2.5 Level" value={189} icon={Icons.wind()} />
+      <DashboardCard title="PM 2.5 Level" value={189} icon={Icons.wind()} />
+      <DashboardCard title="PM 2.5 Level" value={189} icon={Icons.wind()} />
+      <DashboardCard title="PM 2.5 Level" value={189} icon={Icons.wind()} />
     </MaxWidthWrapper>
   );
 }
