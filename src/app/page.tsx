92c2e7c9c82bd3596a84c07a0eee4a5019eb363b
Gear <84141000+macgeargear@users.noreply.github.com>
2024-05-14 22:55:58 +0700

    fix: fix container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,16 @@
 import Dashboard from "@/components/Dashboard";
-import LineChart from "@/components/LineChart";
+import Chart2 from "@/components/GraphData";
 import MaxWidthWrapper from "@/components/MaxwidthWrapper";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <MaxWidthWrapper className="-mt-10">
-        <div className="py-20 mx-auto text-center flex flex-col items-center">
-          <h1 className="text-3xl font-semibold my-8">
-            Embeded System final project
-          </h1>
-          <Dashboard />
-        </div>
-      </MaxWidthWrapper>
-    </main>
+    <MaxWidthWrapper className="-mt-10">
+      <div className="py-20 mx-auto text-center flex flex-col items-center">
+        <h1 className="text-3xl font-semibold mb-8">
+          Embeded System final project
+        </h1>
+        <Dashboard />
+      </div>
+    </MaxWidthWrapper>
   );
 }
