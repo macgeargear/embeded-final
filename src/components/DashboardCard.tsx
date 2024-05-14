c8e351c12bec51b7db41568a7a16b259f7d8edcd
Gear <84141000+macgeargear@users.noreply.github.com>
2024-05-14 22:57:49 +0700

    chore

--- src/components/DashboardCard.tsx
+++ src/components/DashboardCard.tsx
@@ -1,18 +1,17 @@
-import { Wind } from "lucide-react";
+import { getIcon } from "@/lib/utils";
 import React from "react";
 
 type Props = {
   title: string;
   value: number;
-  icon: React.JSX.Element;
 };
 
-export default function DashboardCard({ title, value, icon }: Props) {
+export default function DashboardCard({ title, value }: Props) {
   return (
-    <div className="flex flex-col p-4 border rounded-lg">
+    <div className="flex flex-col p-4 border rounded-xl">
       <div className="flex items-start justify-between">
-        <h1 className="text-xl font-semibold">{title}</h1>
-        {icon}
+        <h1 className="text-lg md:text-xl text-start font-semibold">{title}</h1>
+        {getIcon(title, value)}
       </div>
       <p className="text-4xl self-start">{value}</p>
     </div>
