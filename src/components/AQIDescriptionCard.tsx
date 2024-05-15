44a279b3b51003dc4d22d48e7d03370ccccfcf08
Gear <84141000+macgeargear@users.noreply.github.com>
2024-05-15 17:41:05 +0700

    chore

--- src/components/AQIDescriptionCard.tsx
+++ src/components/AQIDescriptionCard.tsx
@@ -13,27 +13,25 @@ type Props = {
 
 export default function AQIDescriptionCard({ level, desc }: Props) {
   return (
-    <>
-      <Popover>
-        <PopoverButton className="text-sm/6 text-start font-semibold focus:outline-none data-[active]:text-white data-[hover]:text-white data-[focus]:outline-1 data-[focus]:outline-white">
-          {level}
-        </PopoverButton>
-        <Transition
-          enter="transition ease-out duration-200"
-          enterFrom="opacity-0 translate-y-1"
-          enterTo="opacity-100 translate-y-0"
-          leave="transition ease-in duration-150"
-          leaveFrom="opacity-100 translate-y-0"
-          leaveTo="opacity-0 translate-y-1"
+    <Popover>
+      <PopoverButton className="text-sm/6 text-start font-semibold focus:outline-none data-[active]:text-white data-[hover]:text-white data-[focus]:outline-1 data-[focus]:outline-white">
+        {level}
+      </PopoverButton>
+      <Transition
+        enter="transition ease-out duration-200"
+        enterFrom="opacity-0 translate-y-1"
+        enterTo="opacity-100 translate-y-0"
+        leave="transition ease-in duration-150"
+        leaveFrom="opacity-100 translate-y-0"
+        leaveTo="opacity-0 translate-y-1"
+      >
+        <PopoverPanel
+          anchor="bottom"
+          className="p-4 max-w-xs rounded-xl bg-white text-sm/6 [--anchor-gap:var(--spacing-5)] border"
         >
-          <PopoverPanel
-            anchor="bottom"
-            className="p-4 max-w-xs rounded-xl bg-white text-sm/6 [--anchor-gap:var(--spacing-5)] border"
-          >
-            {desc}
-          </PopoverPanel>
-        </Transition>
-      </Popover>
-    </>
+          {desc}
+        </PopoverPanel>
+      </Transition>
+    </Popover>
   );
 }
